0c4bd6879a0839efe790b6bfb873780b0e3b31bc
PrenitaVulpo <52759194+PrenitaVulpo@users.noreply.github.com>
2020-02-20 22:19:19 -0300

    added ejs

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 
+//definindo o ejs como engine de views
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render("home/index")
+});
+
+app.get('/form_noticia', function(req, res){
+    res.render("admin/form_add_noticias")
+});
+
+app.get('/noticias', function(req, res){
+    res.render("noticias/noticias")
+});
 
 //faz com que o express escute a porta informada, também recebe o callback
 app.listen(process.env.PORT || 3000, function () {
